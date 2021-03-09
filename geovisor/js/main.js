b5767d3d4ef232796340c6434c40d5ff833d1b5e
bsoviedoy <63557478+bsoviedoy@users.noreply.github.com>
2021-03-09 14:07:52 -0500

    Add files via upload

--- geovisor/js/main.js
+++ geovisor/js/main.js
@@ -18,20 +18,29 @@ var geoJsonLayer = L.geoJson(data, { //acá se lee el layer del geojson
     onEachFeature: function (feature, layer) { //Y se establece la función para desplegar el popup
       layer.bindPopup("<h2>Detalles</h2><table class='table' border= 1>" +
       "<tr> <td> <strong> Id Informante: </strong> </td> <td>"+ feature.properties.idinformante + "</td> </tr>"+
+      "<tr> <td> <strong> Nombre: </strong> </td> <td>"+ feature.properties.nombre + "</td> </tr>"+
       "<tr> <td> <strong> Palabra </strong> </td> <td>"+ feature.properties.palabra + "</td> </tr>"+
       "<tr> <td> <strong> Modismo </strong> </td> <td>"+ feature.properties.modismo + "</td> </tr>"+
       "<tr> <td> <strong> Edad </strong> </td> <td>"+ feature.properties.edad + "</td> </tr>"+
       "<tr> <td> <strong> Localidad </strong> </td> <td>"+ feature.properties.municipio + " ,"+" " + feature.properties.departamento + " </td> </tr>"+
       "<tr> <td> <strong> Localidad Procedencia </strong> </td> <td>"+ feature.properties.municipio_procedencia + " ,"+" " + feature.properties.departamento_procedencia + "</td> </tr>"+
-      "</table>"); // Se le dice: cuando se haga clic, se despliega esa información
+      "</table>");// Se le dice: cuando se haga clic, se despliega esa información
+
+  //data.setWhere(palabra.value);
+
     }// la función .bindPopup permite el uso de HTML, por lo que se puede personalizar lo que sale en cada dato.
-  });
+  }
+  );
 markers.addLayer(geoJsonLayer);  //y acá se añade cada punto a la variable markers
   
   
 map.addLayer(markers);  // Se añade la variable al mapa
-map.fitBounds(markers.getBounds()); // Y esta si no se para que es :v
+map.fitBounds(markers.getBounds()); 
   
+// Barra de filtrado
+
+
+
 
 // Añade la escala
 L.control.scale({
@@ -59,3 +68,4 @@ var slide= L.control.slideMenu('', {position: 'topright', menuposition: 'toprigh
 width: '30%', height: '400px', delay: '20', icon: 'fas fa-bars'}).addTo(map);
 
 slide.setContents(right + content);
+
